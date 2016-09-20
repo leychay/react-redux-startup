570fd823f7f5939155f82e76f5e95901365410f2
leychay <ikhwan@mohamadikhwan.com>
2016-09-20 09:38:16 +0800

    add console info when listening to env port

--- server.babel.js
+++ server.babel.js
@@ -1,7 +1,21 @@
 import express from 'express';
 
 const app = express();
+var port = 3000;
 
 app.use('/', express.static('public'));
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || port, function(error) {
+  if (error) {
+    console.error(error);
+  } else {
+
+    var listen_port;
+    if (process.env.PORT == undefined) {
+        listen_port = port;
+    } else {
+        listen_port = process.env.PORT;
+    }
+    console.info('==> 🌍  Open up http://localhost:%s/ in your browser.', listen_port);
+  }
+});
