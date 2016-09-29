cd235d691984da5324b208dcab39911211cd3093
leychay <ikhwan@mohamadikhwan.com>
2016-09-29 17:48:49 +0800

    component for PostsLists

---
+++ src/components/PostsList.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+
+class PostsList extends Component {
+    componentWillMount() {
+        this.props.fetchPosts()
+    }
+
+    renderPosts(posts) {
+        return posts.map((post) => {
+            return (
+                <li key={post.id}>
+                <h3>{post.title}</h3>
+                <div>{post.content}</div>
+                </li>
+                )
+        })
+    }
+
+    render() {
+        const { posts, loading, error } = this.props.postsList
+        if(loading) {
+            return <div><h1>Posts</h1><h3>Loading...</h3></div>
+        } else if(error) {
+            return <div>Error: {error.message}</div>
+        }
+
+        return (
+            <div>
+                <h1>Posts</h1>
+                <ul>
+                    {this.renderPosts(posts)}
+                </ul>
+            </div>
+            )
+    }
+}
+
+
+export default PostsList
