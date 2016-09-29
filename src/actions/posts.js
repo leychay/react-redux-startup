c07f26ea30371bbc2d346e0b67eaf93af95cae81
leychay <ikhwan@mohamadikhwan.com>
2016-09-29 17:45:47 +0800

    updated api url
    removed extra semi-colon

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,32 +1,43 @@
 import {
     FETCH_POSTS,
     FETCH_POSTS_SUCCESS,
-    FETCH_POSTS_FAILURE,
-    RESET_POSTS
-} from '../constants/posts';
-import { ROOT_URL } from '../constants/common';
+    FETCH_POSTS_FAILURE
+} from '../constants/posts'
+import {
+    API_GET_POSTS,
+    HTTP_GET
+     } from '../constants/common'
 
-import axios from 'axios';
+import axios from 'axios'
+
 
 export function fetchPosts() {
-    const request = axios.get('${API_URL}/getPosts');
+
+    const request = axios({
+        method: HTTP_GET,
+        url: API_GET_POSTS,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
 
     return {
         type: FETCH_POSTS,
         payload: request
-    };
+    }
 }
 
 export function fetchPostsSuccess(posts) {
     return {
         type: FETCH_POSTS_SUCCESS,
         payload: posts
-    };
+    }
 }
 
-export function fetchPostsFailure(posts) {
+export function fetchPostsFailure(error) {
     return {
         type: FETCH_POSTS_FAILURE,
-        payload: posts
-    };
+        payload: error
+    }
 }
