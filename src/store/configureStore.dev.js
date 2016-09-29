2a747d2967bd5b1b8e14bd7d7b24a7271e32aed3
leychay <ikhwan@mohamadikhwan.com>
2016-09-29 17:50:10 +0800

    added store configuration

---
+++ src/store/configureStore.dev.js
@@ -0,0 +1,25 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import createLogger from 'redux-logger'
+import promise from 'redux-promise'
+import reducer from '../reducers'
+
+
+export default function configureStore(initialState) {
+    const finalCreateStore = compose(
+        applyMiddleware(promise),
+        applyMiddleware(createLogger()),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+      )(createStore)
+
+    const store = finalCreateStore(reducer, initialState)
+
+    if (module.hot) {
+        // Enable Webpack hot module replacement for reducers
+        module.hot.accept('../reducers', () => {
+            const nextReducer = require('../reducers')
+            store.replaceReducer(nextReducer)
+        })
+    }
+
+    return store
+}
