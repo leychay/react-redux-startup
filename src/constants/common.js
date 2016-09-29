aca38f0fdc1b124a3ea1aff5d246e0eb17dd9d33
leychay <ikhwan@mohamadikhwan.com>
2016-09-29 17:46:23 +0800

    updated constants
    removed extra semi-colon

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,11 +1,9 @@
-//ROOT URL
-const ROOT_URL = location.href.indexOf('localhost') > 0 ? 'http://localhost:3000' : '';
-
-//API
-const API_URL = 'http://simple-api.local/api';
-
 //API path
-const API_GET_POSTS = 'getPosts';
-const API_GET_POST = 'getPost';
-const API_CREATE_POST = 'create';
-const API_DELETE_POST = 'delete';
+export const API_GET_POSTS = 'http://simple-api.local/api/getPosts'
+export const API_GET_POST = 'http://simple-api.local/api/getPost'
+export const API_CREATE_POST = 'http://simple-api.local/api/create'
+export const API_DELETE_POST = 'http://simple-api.local/api/delete'
+
+// HTTP Method
+export const HTTP_POST = 'POST'
+export const HTTP_GET = 'GET'
