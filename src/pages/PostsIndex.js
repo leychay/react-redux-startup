a4a96c5c2bdd67ea57518e2d66eb819439a4216b
leychay <ikhwan@mohamadikhwan.com>
2016-09-29 17:49:27 +0800

    added pages for Posts

---
+++ src/pages/PostsIndex.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import PostsListContainer from '../containers/PostsList'
+
+class PostsIndex extends Component {
+
+    render() {
+        return (
+            <div>
+            <PostsListContainer />
+            </div>
+        )
+    }
+}
+
+export default PostsIndex
