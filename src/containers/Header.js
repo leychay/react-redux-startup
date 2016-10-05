551ccfdff350673bd37c735bab60ac3308abcb52
leychay <ikhwan@mohamadikhwan.com>
2016-10-05 13:19:28 +0800

    added deleted post constant

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/posts';
-import { deletePost, deletePostSuccess, deletePostFailure } from '../actions/post';
+import { deletePost, deletePostSuccess, deletePostFailure, resetDeletedPost } from '../actions/post';
 import Header from '../components/Header';
 
 
 
 function mapStateToProps(state) {
   return {
-    deletedPost: state.posts.deletedPost,
-  };
+    deletedPost: state.posts.deletedPost
+  }
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
