10ff193e3fec5f786bfa5072ea61832a11a7782e
leychay <ikhwan@mohamadikhwan.com>
2016-10-05 13:18:45 +0800

    updated API URL

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,10 +1,10 @@
 //API path
 export const API_GET_POSTS = 'http://simple-api.local/api/getPosts'
-export const API_GET_POST = id => `http://simple-api.local/api/getPost/${id}`
+export const API_GET_POST = 'http://simple-api.local/api/getPost/'
 export const API_CREATE_POST = 'http://simple-api.local/api/create'
-export const API_DELETE_POST = id => `http://simple-api.local/api/delete/${id}`
+export const API_DELETE_POST = 'http://simple-api.local/api/delete/'
 
 // HTTP Method
-export const HTTP_POST = 'POST'
-export const HTTP_GET = 'GET'
-export const HTTP_DELETE = 'DELETE'
+export const HTTP_POST = 'post'
+export const HTTP_GET = 'get'
+export const HTTP_DELETE = 'delete'
