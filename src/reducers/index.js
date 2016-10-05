ab190bcb5a7a8a22552d2f1ac515bfded8d4993b
leychay <ikhwan@mohamadikhwan.com>
2016-10-05 13:21:30 +0800

    added post reducer in combineReducer
    added new Route for Post Detail and Create New Post

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 import PostsReducer from './posts'
+import PostReducer from './post'
 import { reducer as formReducer } from 'redux-form'
 
 const rootReducer = combineReducers({
     posts: PostsReducer, //<-- Posts
+    post: PostReducer, //<-- Post
     form: formReducer // <-- redux-form
 })
 
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,13 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
 import Posts from './containers/PostsList'
+import PostNew from './containers/PostNew'
+import Post from './containers/PostDetail'
 
 export default (
   <Route path="/">
     <IndexRoute component={Posts} />
+    <Route path="new" component={PostNew} />s
+    <Route path="show/:id" component={Post} />s
   </Route>
 )
